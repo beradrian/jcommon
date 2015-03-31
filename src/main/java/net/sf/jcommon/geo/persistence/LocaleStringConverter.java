dbe96df1034c87cb2f2cfa39aa3c042a7baae208
Adrian Ber <beradrian@users.noreply.github.com>
2015-03-31 11:05:40 +0000

    new version

--- src/main/java/net/sf/jcommon/geo/persistence/LocaleStringConverter.java
+++ src/main/java/net/sf/jcommon/geo/persistence/LocaleStringConverter.java
@@ -8,12 +8,12 @@ import javax.persistence.Converter;
 @Converter(autoApply = true)
 public class LocaleStringConverter implements AttributeConverter<Locale, String> {
 
-	private static final String SEPARATOR = "_";
-
 	@Override
 	public String convertToDatabaseColumn(Locale attributeObject) {
-		return attributeObject == null ? null : attributeObject.getLanguage() + SEPARATOR 
-				+ attributeObject.getCountry() + SEPARATOR + attributeObject.getVariant();
+	    if (attributeObject == null) {
+	        return null;
+	    }
+		return attributeObject.toLanguageTag();
 	}
 
 	@Override
@@ -21,18 +21,7 @@ public class LocaleStringConverter implements AttributeConverter<Locale, String>
 		if (datastoreValue == null) {
 			return null;
 		}
-		String[] x = datastoreValue.trim().split(SEPARATOR);
-		switch(x.length) {
-			case 1:
-				return new Locale(x[0]);
-			case 2:
-				return new Locale(x[0], x[1]);
-			case 3:
-				return new Locale(x[0], x[1], x[2]);
-			case 0:
-			default:
-				return null;
-		}
+		return Locale.forLanguageTag(datastoreValue.replace('_', '-'));
 	}
 
 }
